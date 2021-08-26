29be291eda5b08979271164a10769fcb9bc4b228
C-Swain <catherinemexicoswain@gmail.com>
2021-08-26 14:01:08 +0000

    now using the for loop

--- packing-list.js
+++ packing-list.js
@@ -5,3 +5,11 @@ for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 
+// using the let loop instead of the for loop to get the same results, running both will result it the lost repeating.
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
